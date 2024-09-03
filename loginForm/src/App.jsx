42ba9bbc6574aafce38ebe4c693d509c864a791c
Sagar Jadhav <sjsagar2000@gmail.com>
2024-09-03 17:55:26 +0530

    Login Sign Up Form Project

---
+++ loginForm/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import LoginSignup from "./components/LoginSignUp/LoginSignup";
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+export default App;
---
+++ loginForm/src/components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,60 @@
+import "./loginSignUp.css";
+import user_icon from "../../assets/person.png";
+import email_icon from "../../assets/email.png";
+import password_icon from "../../assets/password.png";
+import { useState } from "react";
+function LoginSignup() {
+  const [action, setAction] = useState("Sign Up");
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input placeholder="Name" type="text" />
+          </div>
+        )}
+
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input placeholder="Email Id" type="email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input placeholder="Password" type="password" />
+        </div>
+      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost Password ? <span>Click Here!</span>
+        </div>
+      )}
+
+      <div className="submit-container">
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => setAction("Sign Up")}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => setAction("Login")}
+        >
+          Login
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignup;
