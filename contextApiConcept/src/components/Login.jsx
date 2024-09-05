e4e474fb4094cef7d781b5cdac302da22b349085
Sagar Jadhav <sjsagar2000@gmail.com>
2024-09-05 18:07:05 +0530

    Context Api Project

---
+++ contextApiConcept/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUser(username, password);
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleChange}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
